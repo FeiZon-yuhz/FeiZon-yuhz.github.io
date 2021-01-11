abc4c18cfb76705c839b0e643f4f71dba24d8b8c
FeiZon-yuhz <76770654+FeiZon-yuhz@users.noreply.github.com>
2021-01-11 20:51:48 +0800

    彩虹卡

---
+++ rainbowimg.js
@@ -0,0 +1,10 @@
+﻿
+var img = new Array(245);
+for (var i = 0; i < 245; i++) {
+    img[i] = "img/rainbow/n" + i + ".jpg";
+}
+function change() {
+   
+    var num = Math.floor(Math.random() * 245);
+    document.getElementById("rainbowimg").src = img[num];
+}
